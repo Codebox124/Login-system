e4e4f6ed663af7513375cc14f295c32ec4c21d45
codebox124 <rajcodes733@gmail.com>
2023-08-15 10:20:10 -0700

    updated

---
+++ system/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res)=>{
+    res.render('base', {title: "Login System"})
+})
+
+app.listen(port, ()=>{
+    console.log("server started.......!")
+})
